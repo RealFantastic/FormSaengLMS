57ecaa848c1bc104942851ae951d857ec816ce1e
RealFantastic <wjdghks5698@gmail.com>
2022-04-21 00:07:56 +0900

    create Controller / modified template
    테스트용 templateController 생성
    template.jsp / template.css 최종

---
+++ formsaeng_project/src/kh/semi/template/controller/TemplateController.java
@@ -0,0 +1,41 @@
+package kh.semi.template.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TemplateController
+ */
+@WebServlet("/template")
+public class TemplateController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TemplateController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("/template");
+		request.getRequestDispatcher("/WEB-INF/view/template.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+
+}
