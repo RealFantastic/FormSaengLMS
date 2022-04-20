e46c8a27355553d0cbead0459d74d309c60ee89a
hhyrin <hhyrin03@gmail.com>
2022-04-20 10:46:11 +0900

    Create PfLoginDoServlet.java

---
+++ formsaeng_project/src/kh/semi/lms/professor/controller/PfLoginDoServlet.java
@@ -0,0 +1,59 @@
+package kh.semi.lms.professor.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kh.semi.member.model.service.MemberService;
+import kh.semi.member.model.vo.MemberVo;
+
+/**
+ * Servlet implementation class PfLoginDoServlet
+ */
+@WebServlet("/pflogin.do")
+public class PfLoginDoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PfLoginDoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		MemberVo vo = new MemberVo(id, pwd);
+		vo.setId(id);
+		vo.setPwd(pwd);
+		
+		MemberVo result = new MemberService().login(vo);
+		if(result == null) {
+			System.out.println("로그인에 실패했습니다.");
+		}else {
+			System.out.println("로그인에 성공했습니다.");
+			//TODO session 등록
+			response.sendRedirect("mgmain");
+		}
+		
+		
+		
+	}
+
+}
