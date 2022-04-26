af12c0e6d9b239d9f54586e5ffbffedb46a69d18
hhyrin <hhyrin03@gmail.com>
2022-04-26 11:48:06 +0900

    Member 수정

--- formsaeng_project/src/kh/semi/member/model/dao/MemberDao.java
+++ formsaeng_project/src/kh/semi/member/model/dao/MemberDao.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import static kh.semi.lms.common.jdbc.JdbcDbcp.close;
+import static kh.test.first.common.jdbc.JdbcTemplate.close;
+
 import kh.semi.member.model.vo.MemberVo;
 
 public class MemberDao {
@@ -14,13 +16,11 @@ public class MemberDao {
 	private Statement stmt = null;
 	private ResultSet rs = null;
 	
-	
-	
+	// 로그인 - 관리자/교수/학생
 	public MemberVo login(Connection conn, MemberVo vo) {
 		MemberVo result = null;
 		
 		String sql = "select * from member where id=? and pwd=?";
-		
 		System.out.println(vo);
 		try {
 			pstmt = conn.prepareStatement(sql);
@@ -33,6 +33,7 @@ public class MemberDao {
 				System.out.println("메세지 하나");
 				result = vo;
 			}
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -40,6 +41,58 @@ public class MemberDao {
 			close(pstmt);	
 		}
 		System.out.println(result);
+		return result;
+	}
+	
+	// 멤버 정보 조회 (마이페이지) 교수/학생
+	public MemberVo readMember(Connection conn, MemberVo vo) {
+		MemberVo result = null;
+		String sql = "select * from member where id=? and pwd=?";
+		try { 
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getId());
+			pstmt.setString(2, vo.getPwd());
+			rs = pstmt.executeQuery();
+
+			if(rs.next()) {
+				vo.setName(rs.getString("name"));
+				vo.setEmail(rs.getString("email"));
+				vo.setAddress(rs.getString("address"));
+				vo.setDeptCode(rs.getString("deptCode"));
+				vo.setPnum(rs.getString("pnum"));
+				
+				result = vo;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally { 
+			close(rs);
+			close(pstmt);
+		}
+		return result;
+		
+	}
+	
+	// 멤버 정보 수정 (마이페이지) 교수/학생
+	public int updateMember(Connection conn, MemberVo vo) {
+		int result = 0;
+		
+		String sql = "update member set address=?, pnum=? where id=?";
+		try { 
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getAddress());
+			pstmt.setString(2, vo.getPnum());
+			pstmt.setString(3, vo.getId());
+			
+			result = pstmt.executeUpdate();
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally { 
+			close(pstmt);
+		}
 		return result;
 	}
 }
--- formsaeng_project/src/kh/semi/member/model/service/MemberService.java
+++ formsaeng_project/src/kh/semi/member/model/service/MemberService.java
@@ -1,6 +1,6 @@
 package kh.semi.member.model.service;
-
 import static kh.semi.lms.common.jdbc.JdbcDbcp.*;
+
 
 import java.sql.Connection;
 
@@ -8,15 +8,36 @@ import kh.semi.member.model.dao.MemberDao;
 import kh.semi.member.model.vo.MemberVo;
 
 public class MemberService {
+	private MemberDao dao = new MemberDao();
+	
 	public MemberVo login(MemberVo vo) {
 		Connection conn = getConnection();
 		
 		MemberVo result = null;
 
-		result = new MemberDao().login(conn,vo);
+		result = dao.login(conn,vo);
 		
 		close(conn);
 		return result;
 	
 	}
+	
+	public MemberVo readMember(MemberVo vo) {
+		MemberVo result = null;
+		Connection conn = getConnection();
+		result = dao.readMember(conn, vo);
+		
+		close(conn);
+		return result;
+	}
+	
+	public int updateMember(MemberVo vo) {
+		int result = 0;
+		Connection conn = getConnection();
+		result = dao.updateMember(conn, vo);
+		
+		close(conn);
+		return result;
+	}
+	
 }
